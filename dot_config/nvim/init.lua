13284c2f9f242cf951fa6a5a8cca7b94bcc9bc3c
Matias Ceau <matias@ceau.net>
2024-09-05 14:57:38 +0200

    1 change(s) from matias@karjala

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -545,7 +545,7 @@ require("lazy").setup({
 				-- pyright = {},
 				jedi_language_server = {},
 				rust_analyzer = {},
-				bash_ls = {},
+				bashls = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
 				-- Some languages (like typescript) have entire language plugins that can be useful:
