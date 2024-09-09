9a5a50129bec57b077ddd29e7495a6abe6c280c8
Matias Ceau <matias@ceau.net>
2024-09-09 06:09:08 +0200

    1 change(s) from matias@karhu

--- dot_config/qtile/config.py
+++ dot_config/qtile/config.py
@@ -1,17 +1,18 @@
-#from __future__ import annotations
+# from __future__ import annotations
 # from libqtile.log_utils import logger
-from _hooks import *
-#_custom : custom widgets/scripts/pop ups
-#_style : colors, decorations
-from _groups import groups
-from _layouts import layouts
-from _keys import keys, mouse
-from _screens import screens
+import re
 
+# _custom : custom widgets/scripts/pop ups
+# _style : colors, decorations
+from _groups import groups
+from _hooks import *
+from _keys import keys, mouse
+from _layouts import layouts
+from _screens import screens
 from libqtile import layout
 from libqtile.config import Match
 
-terminal = "kitty" #guess_terminal()
+terminal = "kitty"  # guess_terminal()
 
 floating_layout = layout.Floating(
     float_rules=[
@@ -27,7 +28,9 @@ floating_layout = layout.Floating(
         Match(title="fzfmenu"),
         Match(title="floating"),
         Match(title="wallpaper_select"),
-        Match(wm_class="keepassxc")
+        Match(wm_class="keepassxc"),
+        Match(wm_class=re.compile(r".*floating.*")),
+        Match(title=re.compile(r".*floating.*")),
     ]
 )
 
