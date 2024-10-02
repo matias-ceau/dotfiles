f8457af3cf9febddd0a38a33a3e392d58579e99b
Matias Ceau <matias@ceau.net>
2024-10-02 16:55:16 +0200

    4 change(s) from matias@karjala

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -361,57 +361,22 @@ require("lazy").setup({
 						vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
 					end
 
-					-- Jump to the definition of the word under your cursor.
-					--  This is where a variable was first declared, or where a function is defined, etc.
 					--  To jump back, press <C-t>.
 					map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-
-					-- Find references for the word under your cursor.
-					map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-
-					-- Jump to the implementation of the word under your cursor.
-					--  Useful when your language has ways of declaring types without an actual implementation.
-					map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-
-					-- Jump to the type of the word under your cursor.
-					--  Useful when you're not sure what type a variable is and you want to see
-					--  the definition of its *type*, not where it was *defined*.
-					map("<leader>ct", require("telescope.builtin").lsp_type_definitions, "[t]ype definition")
-
-					-- Fuzzy find all the symbols in your current document.
-					--  Symbols are things like variables, functions, types, etc.
-					map("<leader>cs", require("telescope.builtin").lsp_document_symbols, "document [s]ymbols")
-					-- was leader ds
-
-					-- Fuzzy find all the symbols in your current workspace.
-					--  Similar to document symbols, except searches over your entire project.
-					map(
-						"<leader>cS",
-						-- was leader ws
-						require("telescope.builtin").lsp_dynamic_workspace_symbols,
-						"workspace [S]ymbols"
-					)
-
-					-- Rename the variable under your cursor.
-					--  Most Language Servers support renaming across files, etc.
-					map("<leader>r", vim.lsp.buf.rename, "[r]ename variable")
-
-					-- Execute a code action, usually your cursor needs to be on top of an error
-					-- or a suggestion from your LSP for this to activate.
-					map("<leader>ca", vim.lsp.buf.code_action, "code [a]ction", { "n", "x" })
-
-					-- Opens a popup that displays documentation about the word under your cursor
-					--  See `:help K` for why this keymap.
-					map("K", vim.lsp.buf.hover, "Hover Documentation")
-
-					-- WARN: This is not Goto Definition, this is Goto Declaration.
-					--  For example, in C this would take you to the header.
 					map("gD", vim.lsp.buf.declaration, "[g]oto [D]eclaration")
+					map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+					map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+					map("<leader>ct", require("telescope.builtin").lsp_type_definitions, "[t]ype definition")
+					map("<leader>cs", require("telescope.builtin").lsp_document_symbols, "document [s]ymbols")
+					map("<leader>cS", require("telescope.builtin").lsp_dynamic_workspace_symbols, "workspace [S]ymbols")
+
+					map("<leader>r", vim.lsp.buf.rename, "[r]ename variable")
+					map("<leader>ca", vim.lsp.buf.code_action, "code [a]ction", { "n", "x" })
+					map("K", vim.lsp.buf.hover, "Hover Documentation")
 
 					-- The following two autocommands are used to highlight references of the
 					-- word under your cursor when your cursor rests there for a little while.
 					--    See `:help CursorHold` for information about when this is executed
-					--
 					-- When you move your cursor, the highlights will be cleared (the second autocommand).
 					local client = vim.lsp.get_client_by_id(event.data.client_id)
 					if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
@@ -440,7 +405,6 @@ require("lazy").setup({
 
 					-- The following autocommand is used to enable inlay hints in your
 					-- code, if the language server you are using supports them
-					--
 					-- This may be unwanted, since they displace some of your code
 					if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
 						map("<leader>ch", function()
@@ -477,6 +441,8 @@ require("lazy").setup({
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				-- Some languages (like typescript) have entire language plugins that can be useful: https://github.com/pmizio/typescript-tools.nvim
 				-- ts_ls = {},
+				-- ltex = { settings = { enabled = { "bibtex", "tex", "latex" }, }, },
+				texlab = {},
 				lua_ls = {
 					-- cmd = {...},
 					-- filetypes = { ...},
