03d9feccf2cce1b21a5e91fdea098d2027446ec5
Matias Ceau <matias@ceau.net>
2024-10-15 17:42:15 +0200

    removed llm-functions (aichat)

--- dot_config/aichat/functions/agents/demo/tools.js
+++
@@ -1,7 +0,0 @@
-/**
- * Get the system info
- */
-exports.get_ipinfo = async function getIpinfo() {
-   const res = await fetch("https://httpbin.org/ip")
-   return res.json();
-}
--- dot_config/aichat/functions/tools/execute_py_code.py
+++
@@ -1,6 +0,0 @@
-def run(code: str):
-    """Execute the python code.
-    Args:
-        code: Python code to execute, such as `print("hello world")`
-    """
-    return exec(code)
