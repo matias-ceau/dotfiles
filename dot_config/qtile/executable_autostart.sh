f799a8b9b1aa8a0d92c597d639a0db2da729b518
Matias Ceau <matias@ceau.net>
2024-11-03 00:38:51 +0100

    refactor: update model and streamline startup scripts
    - Switch model to Claude 3.5 Sonnet in configuration.
    - Replace two shell scripts with a single Python script in the autostart configuration.

--- dot_config/qtile/executable_autostart.sh
+++ dot_config/qtile/executable_autostart.sh
@@ -16,7 +16,6 @@ start_if_exists nm-applet
 start_if_exists flameshot
 start_if_exists dunst
 start_if_exists picom -b
-start_if_exists launch_tmux_with_music_player.sh
-start_if_exists megacmd_launch_tmux.sh
+start_if_exists tmux_manager.py
 #start_if_exists /usr/bin/emacs --daemon
 #start_if_exists /usr/bin/xonsh ~/scripts/karjala_screen.xsh
