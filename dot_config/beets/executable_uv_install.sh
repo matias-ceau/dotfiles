bf64d9e50c0d0c1c2f20105eedc9a6d5b9d10870
Matias Ceau <matias@ceau.net>
2024-11-06 17:15:32 +0100

    feat: Add encrypted config files and update Beets setup
    - Add encrypted configuration file using PGP.
    - Add encrypted Spotify token for Beets configuration.
    - Remove encrypted secret configuration file.
    - Remove encrypted Spotify token file.
    - Rename configuration file and modify `status` type and activation of `xtractor` plugin.
    - Add new executable simple configuration file template for Beets with various plugins and settings.
    - Downgrade beets installation version from 3.13 to 3.12.

--- dot_config/beets/executable_uv_install.sh
+++ dot_config/beets/executable_uv_install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-uv tool install beets -p 3.13
+uv tool install beets -p 3.12
 cd `uv tool dir` || exit
 cd beets || exit
 uv pip install pyacoustid python3-discogs-client pylast requests
