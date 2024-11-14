0523f710914a87a59d874e92b2376a57b9530295
Matias Ceau <matias@ceau.net>
2024-11-14 23:28:04 +0100

    refactor: update script paths and modify tmux parameter
    - Correct the import order and update the wallpaper script path in Qtile hooks.
    - Modify tmux_manager.py parameter from --all to --auto.

--- dot_config/qtile/_hooks.py
+++ dot_config/qtile/_hooks.py
@@ -1,13 +1,16 @@
-from libqtile import hook
 import os
 import subprocess
+
+from libqtile import hook
+
 
 @hook.subscribe.startup_once
 def autostart():
     auto = os.path.expanduser("~/.config/qtile/autostart.sh")
     subprocess.Popen([auto])
 
+
 @hook.subscribe.startup
 def reset_wallpaper():
-    wall = os.path.expanduser("~/.scripts/wallpaper.sh")
+    wall = os.path.expanduser("~/.local/bin/wallpaper.sh")
     subprocess.Popen([wall])
