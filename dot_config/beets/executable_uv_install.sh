a334669996815606cb9a2b39dc2d32cca22af276
Matias Ceau <matias@ceau.net>
2025-01-07 06:45:13 +0100

    chore: Upgrade beets tool to v3.13.
    - Upgrade beets tool to version 3.13.

--- dot_config/beets/executable_uv_install.sh
+++ dot_config/beets/executable_uv_install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-uv tool install beets -p 3.12
+uv tool install beets -p 3.13
 cd `uv tool dir` || exit
 cd beets || exit
 uv pip install pyacoustid python3-discogs-client pylast requests
