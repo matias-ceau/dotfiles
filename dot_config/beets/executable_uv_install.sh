20718bb2731677e2aa4750f956881cba7458e4f1
Matias Ceau <matias@ceau.net>
2025-01-13 16:43:46 +0100

    chore: update configs and upgrade Beets installation
    - Replace encrypted executable secret configuration with updated PGP message.
    - Remove unused plugins and add new ones in the configuration.
    - Upgrade Beets installation to use the latest version from GitHub and include additional required packages.
    - Correct database file path and enable following inside symbolic links.
    - Add quickmark for Berkley Mood music page.
    - Add key bindings for splitting panes in tmux configuration.
    - Add session file for Qutebrowser with multiple tab histories and active windows.

--- dot_config/beets/executable_uv_install.sh
+++ dot_config/beets/executable_uv_install.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/bash
 
-uv tool install https://github.com/beetbox/beets.git -p 3.13
+uv tool install git+https://github.com/beetbox/beets -p 3.13
 cd `uv tool dir` || exit
 cd beets || exit
-uv pip install pyacoustid python3-discogs-client pylast requests
+uv pip install \
+	pyacoustid \
+	python3-discogs-client \
+	pylast \
+	requests \
+	python-mpd2 \
+	typing_extensions \
+	git+https://github.com/adamjakab/BeetsPluginXtractor
