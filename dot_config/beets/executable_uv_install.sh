a31716dc74f9b37c21a3156ed9848880b914367b
Matias Ceau <matias@ceau.net>
2025-01-28 20:21:21 +0100

    uv exec

--- dot_config/beets/executable_uv_install.sh
+++ dot_config/beets/executable_uv_install.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/bash
 
-uv tool install git+https://github.com/beetbox/beets -p 3.13
-cd `uv tool dir` || exit
-cd beets || exit
-uv pip install \
-	pyacoustid \
-	python3-discogs-client \
-	pylast \
-	requests \
-	python-mpd2 \
-	typing_extensions \
-	git+https://github.com/adamjakab/BeetsPluginXtractor
+uv tool install git+https://github.com/beetbox/beets \
+	-p 3.13 \
+	--with pyacoustid \
+	--with python3-discogs-client \
+	--with pylast \
+	--with requests \
+	--with python-mpd2 \
+	--with typing_extensions \
+	--with git+https://github.com/adamjakab/BeetsPluginXtractor
+
+# cd `uv tool dir` || exit
+# cd beets || exit
+# uv pip install \
+beet
