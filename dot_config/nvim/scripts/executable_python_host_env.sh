6dfa31c93c5d74212fa2b59a901e83955093c970
Matias Ceau <matias@ceau.net>
2025-09-27 05:14:01 +0200

    Update 11 files (+284 -407)
    - dot_environment: +105 -107
    - dot_config/SourceGit/encrypted_preference.json.asc: +61 -60
    - dot_config/environment.d/10-custom.conf: +0 -103
    - dot_bashrc: +12 -85
    - dot_config/fuzzel/fuzzel.ini: +49 -0
    - dot_config/hypr/hyprland.conf: +18 -16
    - dot_config/foot/foot.ini: +28 -5
    - dot_config/tmux/tmux.conf: +1 -17
    - dot_config/qt6ct/qt6ct.conf: +5 -5
    - dot_config/nvim/scripts/executable_python_host_env.sh: +4 -4
    - dot_aliases: +1 -5

--- dot_config/nvim/scripts/executable_python_host_env.sh
+++ dot_config/nvim/scripts/executable_python_host_env.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/bash
 
 NVIM_DATA=$XDG_DATA_HOME/nvim
-PYTHON_NVIM=$NVIM_DATA/python-host-venv
+PYTHON_NVIM_DIR=$NVIM_DATA/python-host-venv
 
-mkdir -p $PYTHON_NVIM
+mkdir -p $PYTHON_NVIM_DIR
 
-[[ ! -d $PYTHON_NVIM ]] &&
+[[ ! -d $PYTHON_NVIM_DIR ]] &&
 uv \
     --directory "$NVIM_DATA" \
     venv python-host-venv \
     --seed &&
 uv \
-    --directory "$PYTHON_NVIM" \
+    --directory "$PYTHON_NVIM_DIR" \
     pip install pynvim openai google-generativeai pip neovim
 
